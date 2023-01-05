1506477882381f6712c6446f146d14699113e610
Alexandr <80934396+Vaso1911@users.noreply.github.com>
2023-01-05 22:02:10 +1100

    re fw

--- js/main.js
+++ js/main.js
@@ -7,6 +7,6 @@ const $wrapper = document.querySelector('.wrapper'),
 
 tl.from($wrapper, {scale: 0,  duration: 1.5})
 .from($logo,{opacity: 0, y: -50, duration: 0.6})
-.from($titleWinter,{opacity: 0, x: -70, duration: 0.6})
+.from($titleWinter,{opacity: 0, x: -50, duration: 0.6})
 .from($titleCollection,{opacity: 0, x: 100, duration: 0.6},"-=0.3")
 .from($text,{opacity: 0, y: -1100, duration: 0.6},"-=1.8")
