2d02e5f0ec8676f3c8766b9bba60f6612f7a4b25
Alexandr <80934396+Vaso1911@users.noreply.github.com>
2023-01-05 19:33:21 +1100

    add

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const $wrapper = document.querySelector('.wrapper'),
+  $logo = document.querySelector('#logo'),
+  $titleWinter = document.querySelector('.text-season'),
+  $titleCollection = document.querySelector('.text-color'),
+  $text = document.querySelector('p'),
+  tl = gsap.timeline();
+
+tl.from($wrapper, {scale: 0,  duration: 1.5})
+.from($logo,{opacity: 0, y: -50, duration: 0.6})
+.from($titleWinter,{opacity: 0, x: -70, duration: 0.6})
+.from($titleCollection,{opacity: 0, x: 100, duration: 0.6},"-=0.3")
+.from($text,{opacity: 0, y: -1100, duration: 0.6},"-=1.8")
